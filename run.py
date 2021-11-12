83d2c3f9ef0a62c53c44531e569c746d77e9253a
DaveyJH <daveyj.dh@googlemail.com>
2021-11-12 10:58:32 +0000

    feat: Add user name input and check function

--- run.py
+++ run.py
@@ -1,44 +1,41 @@
 """Run a quiz game via a CLI"""
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-import requests
 
 
-def check_api_url(difficulty: str):
-    """Retrieve a response from opentdb API.
-
-    Connects to opentdb.com/api to ensure correct query parameters have been
-    used in the URL, allowing for valid data.
-
-    Args:
-        difficulty: The current difficulty level set by the question number
+def get_user_name():
+    """Allows user to input a username
 
     Returns:
-        tuple: (bool, data)
-            bool: True if response from API is 0, else false.
-            data: The response in JSON format.
+        A string as a username.
     """
-    api_url = (
-        "https://opentdb.com/api.php?amount=5&category"
-        f"=18&difficulty={difficulty}&type=multiple"
-    )
-    response = requests.get(api_url)
-    data = response.json()
 
+    while True:
+        print("Characters A-Z, a-z, 0-9 and spaces are permitted.")
+        new_user_name = input("Please enter a user name:\n")
+        if check_user_name(new_user_name):
+            break
+
+    return new_user_name
+
+
+def check_user_name(user_name_str):
+    """Checks username input is valid
+
+    Returns:
+        True if valid, else false.
+    """
+
+    # new_user_name = input("Please enter your user name:\n")
     try:
-        if data['response_code'] != 0:
-            raise ConnectionError(
-                f"Open Trivia Database API URL incorrect: {api_url}\n"
-                f"Response_code from API: {data['response_code']}"
+        if not "".join(user_name_str).isalnum():
+            raise ValueError(
+                "Invalid characters detected"
             )
-    except ConnectionError as e:
-        print(f"Error: {e}")
-        print("Program will now terminate!")
-        exit()
+    except ValueError as e:
+        print(f"Error: {e}, please try again.\n")
+        return False
 
     return True
 
 
-print("Easy:", check_api_url("easy"))
-print("Medium:", check_api_url("medium"))
-print("Hard:", check_api_url("hard"))
-print("Error:", check_api_url("error_string"))
+print(get_user_name())
