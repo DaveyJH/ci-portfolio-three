50b2021decd794d4630495ac5348408496075596
DaveyJH <daveyj.dh@googlemail.com>
2021-11-13 16:16:02 +0000

    feat: Change KEYWORDS to dict and add explanations

--- run.py
+++ run.py
@@ -85,7 +85,10 @@ def print_rules():
     print("\nThis quiz consists of 15 questions. The further you go, the")
     print("harder they become. Unless, of course, you know the answers!")
     print("There is no time limit for the questions, but you do have")
-    print("just one life. One wrong answer and the game is over.\n")
+    print("just one life. One wrong answer and the game is over.\n\n")
+    print("If you answer 5 or 10 questions correctly, your progress is safe.")
+    print("An incorrect answer any time after a safety point will mean your")
+    print("score is equal to the most recent safety point reached.\n")
 
     pause()
 
@@ -138,7 +141,7 @@ def keyword_description(word: str):
         keyword: The keyword chosen by the user.
     """
 
-    def print_title(index):
+    def print_title():
         """Prints the keyword title and an underline of equal length
 
         Args:
@@ -146,11 +149,11 @@ def keyword_description(word: str):
                 be printed.
         """
 
-        print("\n" + f"{KEYWORDS[index].upper()}".center(40))
-        line = "=" * len(KEYWORDS[index])
-        print(f"{line}".center(40))
+        print("\n" + f"{word.upper()}".center(40))
+        line = "=" * len(word)
+        print(f"{line.center(40)}\n")
 
-    def print_content(index):
+    def print_content(description):
         """Prints the content explaining the keyword
 
         Args:
@@ -158,23 +161,64 @@ def keyword_description(word: str):
                 explanation to be printed.
         """
 
-        print(f"\nThe description of {KEYWORDS[index]}")
+        print(f"{description}")
 
-    print_title(KEYWORDS.index(word))
-    print_content(KEYWORDS.index(word))
+    print_title()
+    for description in KEYWORDS[word]:
+        print_content(description)
+    print("")
+    pause()
 
 
 YN = ("y", "n")
 Y = "y"
 N = "n"
-KEYWORDS = (
-    "help",
-    "take",
-    "scores",
-    "review",
-    "even",
-    "call"
-)
+KEYWORDS = {
+    "help": (
+        "The 'help' keyword allows you to return to these handy tips.",
+        "You will be asked if you would like to run through the rules again,",
+        "then if you would like to run through the keywords and their",
+        "functions. Once you are done, you will be returned to the quiz."
+    ),
+    "take": (
+        "The 'take' keyword can be used when you are happy with your",
+        "progress through the quiz and don't feel you can confidently",
+        "continue. You will be asked to confirm your decision. If you do",
+        "confirm, the quiz will end and your score will be recorded if it",
+        "is in the top ten scores. If you change your mind and don't 'take'",
+        "you will be returned to the quiz."
+    ),
+    "scores": (
+        "The 'scores' keyword allows you to view the top 10 scores and the",
+        "user names that achieved them. You will be prompted for an input and",
+        "will subsequently return to the quiz."
+    ),
+    "review": (
+        "This is a one shot keyword!! Once used, it cannot be used again in",
+        "the same quiz!!\nIf you choose to use 'review', you will need to",
+        "confirm your decision. Once you do, you will be presented with",
+        "answers to the current question from 100 people. They may not be",
+        "correct so it is up to you if you take the majority answers or not.",
+        "You will be prompted to return to the question and may then continue."
+    ),
+    "even": (
+        "This is a one shot keyword!! Once used, it cannot be used again in",
+        "the same quiz!!\nIf you choose to use 'even', you will need to",
+        "confirm your decision. Once you do, two of the incorrect answers",
+        "to the current question will be removed. The question will be shown",
+        "again with only the two remaining answers. This will even the odds."
+    ),
+    "call": (
+        "This is a one shot keyword!! Once used, it cannot be used again in",
+        "the same quiz!!\nIf you choose to use 'call', you will need to",
+        "confirm your decision. Once you do, you will be presented with",
+        "a response from a coder companion. They will give you their thoughts",
+        "on the question. However, they may not be correct so it is up to you",
+        "to make the final decision",
+        "You will be prompted to return to the question and may then continue."
+    )
+}
+
 
 print("Welcome!\nAre you clued up enough on code and computers?")
 print("Think you have the knowledge to go all the way?")
@@ -184,7 +228,7 @@ user_name = get_user_name()
 print(f"\nWelcome, {user_name}!")
 
 WANTS_RULES = "Before we begin, should we run through the rules?"
-WANTS_KEYWORDS = "Would you like to know the keywords and their uses?"
+WANTS_KEYWORDS = "Would you like to know a keyword and its function?"
 
 
 def main():
