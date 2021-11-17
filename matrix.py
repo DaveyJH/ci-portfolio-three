9d040092fff5267732da598985709a29ade1be0b
DaveyJH <daveyj.dh@googlemail.com>
2021-11-17 23:48:45 +0000

    ref: Move matrix line creation to matrix.py

---
+++ matrix.py
@@ -0,0 +1,32 @@
+"""Creates matrix style lines of 1s and 0s"""
+
+from random import randrange
+from time import sleep
+
+MATRIX_CHARS = (1, 0)
+
+
+def matrix_line():
+    """Print a random line of 60 1s and 0s followed by a 0.5s delay."""
+
+    line = ""
+    for n in range(80):
+        line = line + (str(MATRIX_CHARS[
+            randrange(len(MATRIX_CHARS))]))
+    print(line)
+    sleep(.5)
+
+
+def matrix_block(num: int = 5):
+    """Print block of matrix lines.
+
+    Functions:
+        matrix_line():
+
+    Args:
+        num (int): (default = 5) Integer value determining how many lines to
+        print.
+    """
+
+    for n in range(num):
+        matrix_line()
