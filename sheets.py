4bc3a14a807f7e743a9fb3d6c03c904846bc3615
DaveyJH <daveyj.dh@googlemail.com>
2021-11-17 23:48:14 +0000

    ref: Move Google Sheets to sheets.py

---
+++ sheets.py
@@ -0,0 +1,19 @@
+"""Handles sheet authoization"""
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("ci_p3_quiz")
+
+TOKEN_SHEET = SHEET.worksheet("tokens")
+SCORES_SHEET = SHEET.worksheet("scores")
