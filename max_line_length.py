04714cdc44b0f7e4723daa4be45734f9b01d214f
DaveyJH <daveyj.dh@googlemail.com>
2021-11-19 17:08:00 +0000

    ref: Add line limit function (80 characters)

---
+++ max_line_length.py
@@ -0,0 +1,30 @@
+"""handles line length of text"""
+
+
+def limit_line_length(string: str):
+    """Prints individual lines with a max line length of 80 characters
+
+    Checks for and prevents prepending spaces/split words"""
+
+    lines = []
+    if len(string) <= 80:
+        lines.append(string)
+    else:
+        while len(string) > 80:
+            string = string.strip()
+            single_line = string[:80]
+            if string[80] == " ":
+                lines.append(single_line.strip())
+                string = string[80:]
+            elif " " in single_line:
+                last_space = single_line.rindex(" ")
+                single_line = string[:last_space]
+                string = string[last_space:].strip()
+                lines.append(single_line)
+            else:
+                lines.append(single_line)
+        # add last line
+        lines.append(string.strip())
+
+    for line in lines:
+        print(line)
